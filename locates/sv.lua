7935089d6c1e2b50fc97025148f4e6657782d57d
FiskN <36334995+xBlueSI@users.noreply.github.com>
2019-07-02 17:39:05 +0200

    Update sv.lua

--- locates/sv.lua
+++ locates/sv.lua
@@ -1,6 +1,3 @@
-Locales['sv'] = {
-	['cartheif_blip']		= 'Biltjuv',
-	['press_e']				= 'Tryck ~INPUT_CONTEXT~',
-	['wrong_vehicle']		= 'Detta är fel fordon!',
-	['mission_completed']	= 'Du har lämnat in fordonet!'
-}
+-- Locales['sv'] = {
+-- 	['mats_blip']		= 'Biltjuv'
+-- }
